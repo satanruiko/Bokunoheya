bdcf4ec01588b3142f3465e0ceb5849ba12e75c2
夏緒さま <48851686+satanruiko@users.noreply.github.com>
2020-10-13 23:33:58 +0800

    添加头像动态变化

--- setting.js
+++ setting.js
@@ -1,13 +1,9 @@
 //我是注释
 "use strict"
-const logo_max=2;
-let logo_img=document.querySelectorAll("#logo");
-let logo_now=0;
-let changelog=(a)=>{
-    let rand_math=Math.ceil(Math.random() * logo_max);
-    if (rand_math===0){changelog();}
-    else {logo_now=rand_math;
-        logo_img.src="${logo_now}.ico";
-        setTimeout(changelog,5000);}
+var logo_f=document.querySelector("#logo");
+function change_por(){
+    var por_now=Math.ceil(Math.random()*5);
+    logo_f.src=`${por_now}.ico`;
+    setTimeout(change_por,10000)
 }
-setTimeout(changelog,5000);
+setTimeout(change_por,10000)
