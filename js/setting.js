221cd4c002a7d85a9542417340d7d948d027fa91
夏緒さま <48851686+satanruiko@users.noreply.github.com>
2020-10-14 07:43:37 +0800

    文件结构更改

--- js/setting.js
+++ js/setting.js
@@ -3,7 +3,7 @@
 var logo_f=document.querySelector("#logo");
 function change_por(){
     var por_now=Math.ceil(Math.random()*5);
-    logo_f.src=`${por_now}.ico`;
-    setTimeout(change_por,10000)
+    logo_f.src=`icon/${por_now}.ico`;
+    setTimeout(change_por,10000);
 }
-setTimeout(change_por,10000)
+setTimeout(change_por,10000);
