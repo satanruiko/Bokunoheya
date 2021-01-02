544ebf332faf08351e64ac39d5fdbbe97959feed
satanruiko <lp932515@gmail.com>
2021-01-02 21:30:59 +0800

    更换内容

--- js/AutoChangeLogo.js
+++
@@ -1,9 +0,0 @@
-//我是注释
-"use strict"
-var logo_f=document.querySelector("#logo");
-function change_por(){
-    var por_now=Math.ceil(Math.random()*6-1);
-    logo_f.src=`icon/${por_now}.ico`;
-    setTimeout(change_por,10000);
-}
-setTimeout(change_por,10000);
